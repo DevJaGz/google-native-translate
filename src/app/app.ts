51bd3c2baf695384b02a8383b294f4deb020e60c
DevJaGz <jgomezpersonal7@gmail.com>
2025-10-27 11:25:05 +0100

    chore: add Angular Material and CDK dependencies, update styles and app template

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,11 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-  <h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
-  <router-outlet></router-outlet>`,
+  template: ` <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <button >
+      <span class="material-symbols-outlined"> mic </span>
+    </button>
+    <router-outlet></router-outlet>`,
   styles: [],
 })
 export class App {}
