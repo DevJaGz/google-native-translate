e0e6913d07013c818e6f66cd0a1b96ba95faeef4
DevJaGz <jgomezpersonal7@gmail.com>
2025-10-27 11:34:05 +0100

    chore: enable inline template for Angular components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: ` <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <button >
+    <button>
       <span class="material-symbols-outlined"> mic </span>
     </button>
     <router-outlet></router-outlet>`,
   styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
