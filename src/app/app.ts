e822d8e38e1bbc54ea7c064d9669806565a16056
DevJaGz <jgomezpersonal7@gmail.com>
2025-10-27 18:19:30 +0100

    chore: add Tailwind CSS and Angular Material theming support, update styles and app template

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: ` <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <button>
+    <button class="bg-neutral-500">
       <span class="material-symbols-outlined"> mic </span>
     </button>
     <router-outlet></router-outlet>`,
