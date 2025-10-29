1c5631ebc16f3f9977e21839e98239586d33a56a
DevJaGz <jgomezpersonal7@gmail.com>
2025-10-29 19:03:43 +0100

    feat: add IconButton, Icon, LanguageSelectorButton, LanguageSelectorModal, TabButton components with tests; update index exports

--- src/app/shared/ui/index.ts
+++ src/app/shared/ui/index.ts
@@ -1,5 +1,1 @@
-export * from './icon/icon';
-export * from './icon-button/icon-button';
-export * from './logo/logo';
-export * from './tab-button/tab-button';
-export * from './language-selector-button/language-selector-button';
+export * from './components';
